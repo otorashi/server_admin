0955fdcffbe4569648c93a6747dd653a21d53f77
otorashi <fthealma@gmail.com>
2021-02-18 01:16:43 -0800

    update php 2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "172.19.0.3";
+$servername = "172.19.0.2";
 $username = "root";
 $password = "abcde";
 $dbname = "Trucorp";
@@ -14,12 +14,14 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM users";
 $result = $conn->query($sql);
 
+$totaluser = $result->num_rows;
+
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     echo "id: " . $row["id"]. "<br> - Name: " . $row["nama"]. "<br> kantor " . $row["kantor"]. "<br> <br>";
   }
-  echo $result->num_rows;
+  echo $totaluser;
 } else {
   echo "no results";
 }
